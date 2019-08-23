e33f157e78d9d4f26e66998e2f0f685061c26af3
Rithaven <veneruth4@gmail.com>
2019-08-23 17:18:02 +0200

    boots

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -13,17 +13,20 @@ export class QuoteComponent implements OnInit {
     {id:1, name:'You only live once, but if you do it right, once is enough.',description:'_Mae West'},
     {id:2,name:'Be the change that you wish to see in the world.',description:'_Mahatma Gandhi'},
     {id:3,name:'In three words I can sum up everything I have learned about life: it goes on.',description:'_Robert Frost'},
-    {id:4,name:'No one can make you feel inferior without your consent.',description:'_Eleanor Roosevelt, This is her My Story'},
+    {id:4,name:'No one can make you feel inferior without your consent.',description:'_Eleanor Roosevelt'},
     {id:5,name:"If you tell the truth, you don't have to remember anything.",description:'_Mark Twain'},
-    {id:6,name:'Without music, life would be a mistake.',description:'_Friedrich Nietzsche, Twilight of the Idols'},
+    {id:6,name:'Without music, life would be a mistake.',description:'_Friedrich Nietzsche'},
    ]
    
    deleteQuote(isComplete, index){
     if (isComplete) {
-      this.quotes.splice(index,1);
+      let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].description}'quote?`)
+
+      if (toDelete){
+        this.quotes.splice(index,1)
+      }
     }
   }
-
   
   numberOfLikes: number = 0;
 
