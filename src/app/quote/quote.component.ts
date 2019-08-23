02ff07114318caa9f2d3e939cf32f062541dffc3
Rithaven <veneruth4@gmail.com>
2019-08-23 13:07:42 +0200

    so far

---
+++ src/app/quote/quote.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
+
+@Component({
+  selector: 'app-quote',
+  templateUrl: './quote.component.html',
+  styleUrls: ['./quote.component.css']
+})
+export class QuoteComponent implements OnInit {
+
+  quotes:Quote[] = [
+    {id:1, name:'You only live once, but if you do it right, once is enough.',description:'_Mae West'},
+    {id:2,name:'Be the change that you wish to see in the world.',description:'_Mahatma Gandhi'},
+    {id:3,name:'In three words I can sum up everything I have learned about life: it goes on.',description:'_Robert Frost'},
+    {id:4,name:'No one can make you feel inferior without your consent.',description:'_Eleanor Roosevelt, This is her My Story'},
+    {id:5,name:"If you tell the truth, you don't have to remember anything.",description:'Mark Twain'},
+    {id:6,name:'Without music, life would be a mistake.',description:'Friedrich Nietzsche, Twilight of the Idols'},
+   ]
+   
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
