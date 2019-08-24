10a61e42ecae96d90fea9020e3edf1d4db16e86c
Rithaven <veneruth4@gmail.com>
2019-08-24 13:54:52 +0200

    allthese

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -8,7 +8,7 @@ import { Alert } from 'selenium-webdriver';
   styleUrls: ["./quote-form.component.css"]
 })
 export class QuoteFormComponent implements OnInit {
-  newQuote = new Quote();
+  newQuote = new Quote(0,"","","");
   @Output() addQuote = new EventEmitter<Quote>();
 
   submitQuote(){
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,21 +9,23 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
 
-  quotes: Quote[]= [
-    new Quote(1, 'You only live once, but if you do it right, once is enough.', '_Mae West'),
-    new Quote(2,'Be the change that you wish to see in the world.','_Mahatma Gandhi'),
-    new Quote(3,'In three words I can sum up everything I have learned about life: it goes on.','_Robert Frost'),
-    new Quote(4,'No one can make you feel inferior without your consent.','_Eleanor Roosevelt'),
-    new Quote(5,"If you tell the truth, you don't have to remember anything.",'_Mark Twain'),
-    new Quote(6,'Without music, life would be a mistake.','_Friedrich Nietzsche'),
-   ];
+  quotes: Quote[] = [
+    new Quote(1, 'You only live once, but if you do it right, once is enough.', '_Mae West',''),
+    new Quote(2,'Be the change that you wish to see in the world.','_Mahatma Gandhi',''),
+    new Quote(3,'In three words I can sum up everything I have learned about life: it goes on.','_Robert Frost',''),
+    new Quote(4,'No one can make you feel inferior without your consent.','_Eleanor Roosevelt',''),
+    new Quote(5,"If you tell the truth, you don't have to remember anything.",'_Mark Twain',''),
+    new Quote(6,'Without music, life would be a mistake.','_Friedrich Nietzsche',''),
+  ]
    addNewQuote(quote){
     let quoteLength = this.quotes.length;
     quote.id = quoteLength+1;
     // quote.completeDate = new Date(quote.completeDate)
     this.quotes.push(quote)
   }
-  
+  toggleDetails(index){
+    this.quotes[index].showDescription = !this.quotes[index].showDescription;
+  }
    deleteQuote(isComplete, index){
     if (isComplete) {
       let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].description}'quote?`)
@@ -34,14 +36,14 @@ export class QuoteComponent implements OnInit {
     }
   }
   
-  // numberOfLikes: number = 0;
+  numberOfLikes: number = 0;
 
-  // likeButtonClick(){
-  //  this.numberOfLikes++;
-  // }
-  // dislikeButtonclick(){
-  // this.numberOfLikes--;
-  // }
+  likeButtonClick(){
+   this.numberOfLikes++;
+  }
+  dislikeButtonclick(){
+  this.numberOfLikes--;
+  }
   constructor() { }
 
   ngOnInit() {
