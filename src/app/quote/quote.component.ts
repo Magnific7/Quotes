06d62ba13f0044ecdd5a1be0c1f3325611aa9e7f
Rithaven <veneruth4@gmail.com>
2019-08-24 11:45:52 +0200

    these

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,15 +9,21 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
 
-  quotes:Quote[] = [
-    {id:1, name:'You only live once, but if you do it right, once is enough.',description:'_Mae West'},
-    {id:2,name:'Be the change that you wish to see in the world.',description:'_Mahatma Gandhi'},
-    {id:3,name:'In three words I can sum up everything I have learned about life: it goes on.',description:'_Robert Frost'},
-    {id:4,name:'No one can make you feel inferior without your consent.',description:'_Eleanor Roosevelt'},
-    {id:5,name:"If you tell the truth, you don't have to remember anything.",description:'_Mark Twain'},
-    {id:6,name:'Without music, life would be a mistake.',description:'_Friedrich Nietzsche'},
-   ]
-   
+  quotes: Quote[]= [
+    new Quote(1, 'You only live once, but if you do it right, once is enough.', '_Mae West'),
+    new Quote(2,'Be the change that you wish to see in the world.','_Mahatma Gandhi'),
+    new Quote(3,'In three words I can sum up everything I have learned about life: it goes on.','_Robert Frost'),
+    new Quote(4,'No one can make you feel inferior without your consent.','_Eleanor Roosevelt'),
+    new Quote(5,"If you tell the truth, you don't have to remember anything.",'_Mark Twain'),
+    new Quote(6,'Without music, life would be a mistake.','_Friedrich Nietzsche'),
+   ];
+   addNewQuote(quote){
+    let quoteLength = this.quotes.length;
+    quote.id = quoteLength+1;
+    // quote.completeDate = new Date(quote.completeDate)
+    this.quotes.push(quote)
+  }
+  
    deleteQuote(isComplete, index){
     if (isComplete) {
       let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].description}'quote?`)
@@ -28,14 +34,14 @@ export class QuoteComponent implements OnInit {
     }
   }
   
-  numberOfLikes: number = 0;
+  // numberOfLikes: number = 0;
 
-  likeButtonClick(){
-   this.numberOfLikes++;
-  }
-  dislikeButtonclick(){
-  this.numberOfLikes--;
-  }
+  // likeButtonClick(){
+  //  this.numberOfLikes++;
+  // }
+  // dislikeButtonclick(){
+  // this.numberOfLikes--;
+  // }
   constructor() { }
 
   ngOnInit() {
